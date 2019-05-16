632e54a290b6a22e1c2e01199816700b9b0e6f3c
Vgnttv <o.vignani@gmail.com>
2019-05-16 19:11:47 +0200

    implemented Router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,34 @@
 import React from "react";
 import { Provider } from "react-redux";
-import { Route } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 import store from "./store";
-import SignupFormContainer from "./Components/Signup/signupFormContainer";
-import LoginContainer from "./Components/Login/loginContainer";
-import ProductList from "./Components/Product/productListPage";
-import ProductDetails from "./Components/Product/productPage";
-import ShopList from "./Components/Shop/shopListPage";
-import ShopDetails from "./Components/Shop/shopPage";
-import CategoryList from "./Components/Category/categoryListPage";
-
+// import SignupFormContainer from "./Components/Signup/signupFormContainer";
+// import LoginContainer from "./Components/Login/loginContainer";
+// import ProductList from "./Components/Product/productListPage";
+// import ProductDetails from "./Components/Product/productPage";
+// import ShopList from "./Components/Shop/shopListPage";
+// import ShopDetails from "./Components/Shop/shopPage";
+// import CategoryList from "./Components/Category/categoryListPage";
+import Router from "./Router";
+const Navigation = props => (
+  <nav>
+    <ul>
+      <li>
+        <NavLink to="/shops">Shops</NavLink>
+      </li>
+      <li>
+        <NavLink to="/products/1">Products</NavLink>
+      </li>
+    </ul>
+  </nav>
+);
 function App() {
   return (
     <div>
       <Provider store={store}>
-        <div>
+        <Navigation />
+        <Router />
+        {/* <div>
           <Route path="/register" exact component={SignupFormContainer} />
           <Route path="/logins" exact component={LoginContainer} />
           <Route path="/products" exact component={ProductList} />
@@ -23,7 +37,7 @@ function App() {
           <Route path="/shops/:shopId" exact component={ShopDetails} />
           <Route path="/products/:productId" exact component={ProductDetails} />
           <Route path="/categories" exact component={CategoryList} />
-        </div>
+        </div> */}
       </Provider>
     </div>
   );
---
+++ src/Router.js
@@ -0,0 +1,25 @@
+import React from "react"
+import {Switch, Route } from "react-router-dom"
+import SignupFormContainer from "./Components/Signup/signupFormContainer";
+import LoginContainer from "./Components/Login/loginContainer";
+import ProductList from "./Components/Product/productListPage";
+import ProductDetails from "./Components/Product/productPage";
+import ShopList from "./Components/Shop/shopListPage";
+import ShopDetails from "./Components/Shop/shopPage";
+import CategoryList from "./Components/Category/categoryListPage";
+const Router = () => (
+    <Switch>
+        {/* <Route exact path ="/" component = {LandingPage}/> */}
+        <Route path="/register" exact component={SignupFormContainer} />
+          <Route path="/logins" exact component={LoginContainer} />
+          <Route path="/products" exact component={ProductList} />
+
+          <Route path="/shops" exact component={ShopList} />
+          <Route path="/shops/:shopId" exact component={ShopDetails} />
+          <Route path="/products/:productId" exact component={ProductDetails} />
+          <Route path="/categories" exact component={CategoryList} />
+    </Switch>
+
+)
+
+export default Router
