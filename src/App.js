48988fa9410efcb7d71a48e0bbdbdd2467e59adf
Vgnttv <o.vignani@gmail.com>
2019-05-17 10:00:37 +0200

    navigation into component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,7 @@
 import React from "react";
-import { NavLink } from "react-router-dom";
-
+import Navigation from "./Components/Navigation/navigationPage";
 import Router from "./Router";
-const Navigation = props => (
-  <nav>
-    <ul>
-      <li>
-        <NavLink to="/">Home</NavLink>
-      </li>
-      <li>
-        <NavLink to="/shops">Shops</NavLink>
-      </li>
-      <li>
-        <NavLink to="/logins">Login</NavLink>
-      </li>
-      <li>
-        <NavLink to="/register">Sign up</NavLink>
-      </li>
-      <li>
-        <NavLink to="/logout">Logout</NavLink>
-      </li>
-    </ul>
-  </nav>
-);
+
 function App() {
   return (
     <div>
