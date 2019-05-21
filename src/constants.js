f8b56d893d2908c2919c75713e3e54f8a04e3a23
robertlangereis <robert.langereis@gmail.com>
2019-05-21 10:31:23 +0200

    workaround localstorage to save the current-user info in state

--- src/constants.js
+++ src/constants.js
@@ -2,3 +2,14 @@ export const baseUrl = "http://grozeries.herokuapp.com";
 // export const baseUrl = "localhost:4000";
 
 export const localStorageJwtKey = "currentUserJwt";
+export let localStorageFirstName = "first";
+export let localStorageLastName = "last";
+export let localStorageStreet = "street";
+export let localStorageHouse = "house";
+export let localStorageZip = "zip";
+export let localStorageCity = "city";
+export let localStoragePhone = "phone";
+export let localStorageEmail = "email";
+
+
+console.log("localStorageFirstName", localStorageFirstName)
--- src/middleware.js
+++ src/middleware.js
@@ -1,11 +1,19 @@
 import { USER_LOGIN_SUCCESS, USER_LOGOUT } from "./actions/users";
-import { localStorageJwtKey } from "./constants";
+import { localStorageJwtKey, localStorageFirstName, localStorageLastName, localStorageCity, localStorageEmail, localStorageHouse, localStoragePhone, localStorageStreet, localStorageZip } from "./constants";
 
 export const storeJwt = store => next => action => {
   try {
     if (action.type === USER_LOGIN_SUCCESS) {
-      console.log("loginnnnnnnnn");
+      console.log("loginnnnnnnnn", action.payload);
       localStorage.setItem(localStorageJwtKey, action.payload.jwt);
+      localStorage.setItem(localStorageFirstName, action.payload.first_name);
+      localStorage.setItem(localStorageLastName, action.payload.last_name);
+      localStorage.setItem(localStorageStreet, action.payload.street_name);
+      localStorage.setItem(localStorageHouse, action.payload.house_number);
+      localStorage.setItem(localStorageZip, action.payload.zipcode);
+      localStorage.setItem(localStorageCity, action.payload.city);
+      localStorage.setItem(localStoragePhone, action.payload.phonenumber);
+      localStorage.setItem(localStorageEmail, action.payload.email);
     }
     if (action.type === USER_LOGOUT) {
       localStorage.removeItem(localStorageJwtKey);
