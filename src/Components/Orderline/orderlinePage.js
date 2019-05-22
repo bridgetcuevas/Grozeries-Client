883a309640f14998f3481acb5a49e9703a69c567
Vgnttv <o.vignani@gmail.com>
2019-05-22 13:58:35 +0200

    products image and name in Cart

--- src/Components/Orderline/orderlinePage.js
+++ src/Components/Orderline/orderlinePage.js
@@ -2,21 +2,19 @@ import React from "react";
 
 export default props => {
   const { id, price, quantity, orderId, shopId, productId } = props.orderline;
-  // const { image, product_name } = props.product;
+  const { image, product_name } = props.orderline.product;
   // const { shop_name } = props.shop;
 
   return (
     <div key={id}>
       <p>Your order number: {orderId}</p>
-      <p>{productId} </p>
-      {/* display product name and product image
       <p>{product_name} </p>
-      <img src={image} alt="product" width="300" /> */}
+      <img src={image} alt="product" width="300" />
       <p>Quantity: {quantity}</p>
       <p>From: {shopId}</p>
       {/* display shop name
       <p>From: {shop_name} </p> */}
-      <p>Price: {price}</p>
+      <p>Price: € {price}</p>
     </div>
   );
 };
