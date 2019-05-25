57c24f945fca3db4eca878ad41f85e37afbe6cda
robertlangereis <robert.langereis@gmail.com>
2019-05-25 16:25:41 +0200

    when adding a new product to your order. It first looks for the order based on your user id. If you do not have an existing order (of which payment is not OK), it will create a new one for you. It then creates a new orderline and adds this to the order created. Else the newly created orderline is added to your existing order.

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
-export const baseUrl = "http://grozeries.herokuapp.com";
-// export const baseUrl = 'http://localhost:4000'
+// export const baseUrl = "http://grozeries.herokuapp.com";
+export const baseUrl = 'http://localhost:4000'
 
 export const localStorageJwtKey = "currentUserJwt";
 export let localStorageFirstName = "first";
